7b2336db54c3ec53202265a6d3dcd058e29b9b20
Jed Duffey <jedduffey@gmail.com>
2024-10-05 11:35:13 -0400

    Set Up MongoDB Connection

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/casino_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB', err);
+});
+
 // Middleware
 app.use(bodyParser.json());
 
